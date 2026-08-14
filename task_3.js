9955828dc5f34bf17dec42ef0076b5fb56bc7c46
Mayank1903mishra <mayank.25b15310216@abes.ac.in>
2026-08-14 11:18:46 +0530

    Add files via upload

---
+++ task_3.js
@@ -0,0 +1,11 @@
+function percentage(m){
+    return (m/300)*100
+
+}
+let maths =99
+let english =89
+let science =88
+var m=maths+english+science
+console.log('totl percentage is: ',percentage(m),'%');
+
+
