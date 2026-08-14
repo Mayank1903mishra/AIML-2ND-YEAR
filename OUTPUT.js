6022dfe5860ad46536df89ecf1e17e5fd858d580
Mayank1903mishra <mayank.25b15310216@abes.ac.in>
2026-08-14 11:19:19 +0530

    Add files via upload

---
+++ OUTPUT.js
@@ -0,0 +1,9 @@
+let name ='Mayak mishra'
+const id= 250315300111
+ let branch ='cse - AIML'
+ var department = 'aiml-a'
+ console.log(name);
+ console.log(branch);
+ console.log(id);
+ console.log(department);
+ 
